8f7c3cb85e2dd985cd28b3c68b3557d9a30553ae
R_a_v_i_n <numeshravindra2003@gmail.com>
2025-08-19 21:51:15 +0530

    Photography Web

---
+++ Photography.html/Codes/script.js
@@ -0,0 +1,7 @@
+        function signWork() {
+            alert("Wellcome My Page");
+        }
+        function Event() {
+            alert("Send me an E mail messege (numeshravindra20003@gmail.com) ");
+        }
+
